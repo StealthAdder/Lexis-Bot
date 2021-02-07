2d5a232ccd00858bb7c0ad46a77eb9ee0ce3d864
StealthAdder <hariharanp2435@gmail.com>
2021-02-07 22:29:59 +0530

    GIFs for Weather Animation.
    Signed-off-by: StealthAdder <hariharanp2435@gmail.com>

--- commands/weather.js
+++ commands/weather.js
@@ -33,50 +33,48 @@ module.exports.run = async (client,message,args,messageArray) => {
             function switchResult(icon) {
                 switch(icon) {
                     case "01n":
-                        return ":night_with_stars:";
                     case "01d":
-                        // clear sky
-                        return ":sunny:";
+                        return "https://s2.gifyu.com/images/Clear.gif";
 
                     case "02n":
                     case "02d":
                         // clouds
-                        return ":cloud::cloud:";
+                        return "https://s2.gifyu.com/images/few-clouds.gif";
 
                     case "03n":
                     case "03d":
                         // scattered clouds
-                        return ":cloud::white_sun_small_cloud:";
+                        return "https://s2.gifyu.com/images/Mostly-Sunny.gif";
                     
                     case "04n":
                     case "04d":
                         // broken clouds
-                        return ":cloud::zap::cloud_rain:";
+                        return "https://s2.gifyu.com/images/Cloudly.gif";
 
                     case "09n":
                     case "09d":
                         // shower rain
-                        return ":white_sun_rain_cloud:";
+                        return "https://s2.gifyu.com/images/Rain3797367f06a42b15.gif";
                     
                     case "10n":
                     case "10d":
                         // rain
-                        return ":cloud_rain:";
+                        return "https://s2.gifyu.com/images/Rain3797367f06a42b15.gif";
 
                     case "11n":
                     case "11d":
                         // thunderstorm
-                        return ":thunder_cloud_rain:";
+                        return "https://s2.gifyu.com/images/ThunderStorm.gif";
 
                     case "13n":
                     case "13d":
                         // snow
-                        return ":cloud_snow::snowflake:";
+                        return "https://s2.gifyu.com/images/Snow.gif";
 
                     case "50n":
                     case "50d":
                         // mist
-                        return ":cloud:";
+                        return "https://s2.gifyu.com/images/Hazy.gif";
 
                     default :
                         return ":warning: Error";
@@ -90,8 +88,9 @@ module.exports.run = async (client,message,args,messageArray) => {
             .setColor(0x4c8dcf)
             .addField("Descritption", `${description.toUpperCase()}`)
             .addField("Temperature", `:thermometer: ${temp}°F`)
-            .addField("Climate Condition", `${cli}`)
-            .addField("Humidity", `:droplet:${humidity}%`);
+            .setImage(cli)
+            .addField("Humidity", `:droplet:${humidity}%\n**Climate Condition**`)
+            
             message.channel.send(embed);
         }
     })
